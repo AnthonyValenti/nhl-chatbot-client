0f1a878ea62ba3ad32b89df43a835ff3df3e458e
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-12-05 18:07:51 -0500

    updated icons

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       const jsonData = {
         prompt: prompt
       };
-      const res = await fetch('http://127.0.0.1:8000/chat', {
+      const res = await fetch('https://chatgm-server.onrender.com/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
