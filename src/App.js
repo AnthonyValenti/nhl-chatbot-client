c2e25cdba659b2a24ce51770425f4d2e5704639b
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-12-05 14:45:49 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import arrow from './up-arrow.png';
+import clear from './clear.png';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import {Rings} from 'react-loading-icons'
+
 
 function App() {
+  const [chatArray, setChatArray] = useState([]);
+  const [userMsg,setUserMsg] = useState("");
+  const [responseMsg,setResponseMsg] = useState("");
+  const [isLoadingRes, setIsLoadingRes] = useState(false);
+
+
+  const handleInput = (event) => {
+    setUserMsg(event.target.value);
+  };
+
+  const handleClear = () => {
+    setChatArray([]);
+  }
+
+  const submitMsg = async () => {
+    const newDiv = <div key={chatArray.length} className='msg'><p className='role'>You</p>{userMsg}</div>;
+    setUserMsg("");
+    setChatArray((prevChatArray) => [...prevChatArray, newDiv]);
+    setIsLoadingRes(true);
+    await getResponse(userMsg)  }
+
+  const checkEnter = async (event) => {
+    if(event.key==='Enter'){
+      submitMsg();
+    }
+  };
+
+  const getResponse = async(prompt) =>{
+    try{
+      const jsonData = {
+        prompt: prompt
+      };
+      const res = await fetch('http://127.0.0.1:8000/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+      });
+      if (!res.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const response = await res.json();
+      setResponseMsg(response);
+      const newDiv = <div key={chatArray.length} className='msg'><p className='role'>ChatGM</p>{response}</div>;
+      console.log(responseMsg)
+      setChatArray((prevChatArray) => [...prevChatArray, newDiv]);
+    }catch (error) {
+      // Handle errors here
+      console.error('Error sending data:', error);
+    }finally{
+      setIsLoadingRes(false);
+    }
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatGM</h1>
+      <div className='chat'>
+      {chatArray.map((message) => (
+          <div key={message.key}>{message}</div>
+        ))}
+        {isLoadingRes && <div className='msg'><p className='role'>ChatGM</p><Rings/></div>}</div>
+
+      <div className="bottomBar">
+        <input className="textInput" placeholder='Ask chatGM...' onKeyDown={checkEnter} onChange={handleInput} value={userMsg}/>
+        <button className='arrow' onClick={submitMsg}><img src={arrow} alt='arrow'></img></button>
+        <button className='clear' onClick={handleClear}><img src={clear} alt='clear'></img></button>
+      </div>
     </div>
   );
 }
